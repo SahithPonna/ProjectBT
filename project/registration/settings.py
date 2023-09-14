022a6f0952448ef137e3bea9ab9804b17bdfd197
SahithPonna <84504257+SahithPonna@users.noreply.github.com>
2023-09-14 20:04:29 +0530

    Update settings.py

--- project/registration/settings.py
+++ project/registration/settings.py
@@ -1,3 +1,4 @@
+#settings.py
 """
 Django settings for registration project.
 
@@ -14,7 +15,7 @@ import os
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(_file_)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -121,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
